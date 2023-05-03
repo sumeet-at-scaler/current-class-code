5b350bd5fa5dd20d7494d405dd0fb4810c8ae26a
sumeet-at-scaler <sumeet@scaler.com>
2023-05-03 22:48:37 +0530

    3rd push for 3rd may

---
+++ 3rd May 2023/Demo3/Client.java
@@ -0,0 +1,14 @@
+package FairWork.Lecture4_Inter_OOPs.Demo3;
+
+public class Client {
+    public static void main(String[] args) {
+        final int x;
+        int y = 20;
+
+        x = 100;
+//        x++;
+//        x = 23;
+
+        System.out.println(x);
+    }
+}
---
+++ 3rd May 2023/Demo3/DemoClass.java
@@ -0,0 +1,10 @@
+package FairWork.Lecture4_Inter_OOPs.Demo3;
+
+public class DemoClass {
+    final int x = 10;
+    final int y;
+
+    DemoClass(){
+        y = 100;
+    }
+}
